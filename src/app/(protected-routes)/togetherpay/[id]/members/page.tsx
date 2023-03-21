84562b87586cbaf2c15a97bd7678d0e35d5e1939
Andrés Court <andres@andrescourt.com>
2023-03-21 16:09:26 -0500

    Created a layout for the single group pages

--- src/app/(protected-routes)/togetherpay/[id]/members/page.tsx
+++ src/app/(protected-routes)/togetherpay/[id]/members/page.tsx
@@ -1,6 +1,3 @@
-import Button from "@/components/ui/Button";
-import Headline2 from "@/components/ui/headlines/Headline2";
-import { Plus } from "lucide-react";
 import Link from "next/link";
 import { FC } from "react";
 
@@ -11,22 +8,7 @@ interface PageProps {
 const Page: FC<PageProps> = ({ params }) => {
   return (
     <>
-      <div className="md:mt-20 md:h-1/2 bg-navy-800 text-navy-50 flex flex-col justify-center items-center">
-        <Headline2 className="text-navy-50 pt-10 uppercase tracking-widest">
-          california trip
-        </Headline2>
-        <p className="mt-6 text-base">
-          Transactions: <strong>23</strong>
-        </p>
-        <p className="text-base">
-          Members: <strong>6</strong>
-        </p>
-        <p className="text-base text-brand-700">
-          Total due: <strong>$57.21</strong>
-        </p>
-        <Button className="mt-6">
-          <Plus className="h-4 w-4" /> new expense
-        </Button>
+      <div className="bg-navy-800 text-navy-50 flex flex-col justify-center items-center">
         <div className="mt-6 gap-4 text-xs md:text-base md:w-2/6 py-1 flex items-start justify-between">
           <Link href={`/togetherpay/${params.id}`}>Transactions</Link>
           <Link href={`/togetherpay/${params.id}/debt`}>Debt</Link>
@@ -34,7 +16,7 @@ const Page: FC<PageProps> = ({ params }) => {
           <Link href={`/togetherpay/${params.id}/recent`}>Recent activity</Link>
         </div>
       </div>
-      <div>Page</div>
+      <div>Members</div>
     </>
   );
 };
