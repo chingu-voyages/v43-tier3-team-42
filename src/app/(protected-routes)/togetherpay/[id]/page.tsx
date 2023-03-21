7f832d6010a9f003534c3b1c4dd79c3272a8eeed
Andrés Court <andres@andrescourt.com>
2023-03-21 18:03:12 -0500

    Individual group transactions page

--- src/app/(protected-routes)/togetherpay/[id]/page.tsx
+++ src/app/(protected-routes)/togetherpay/[id]/page.tsx
@@ -1,13 +1,30 @@
+import { Search } from "lucide-react";
 import Link from "next/link";
 import { FC } from "react";
+import * as expenses from "@/lib/expenses.json";
+import ExpenseDetail from "@/components/ExpenseDetail";
 
 interface PageProps {
   params: { id: string };
 }
 
 const Page: FC<PageProps> = ({ params }) => {
+  const displayData = expenses.map((expense) => {
+    console.dir(expense);
+    return (
+      <ExpenseDetail
+        key={expense.id}
+        type={expense.type}
+        total={expense.total}
+        memberName={expense.member.name}
+        description={expense.description}
+        date={expense.date}
+      />
+    );
+  });
+
   return (
-    <>
+    <div>
       <div className="bg-navy-800 text-navy-50 flex flex-col justify-center items-center">
         <div className="mt-6 gap-4 text-xs md:text-base md:w-2/6 py-1 flex items-start justify-between">
           <p className="border-b-2 border-b-navy-50 pb-2">Transactions</p>
@@ -16,8 +33,20 @@ const Page: FC<PageProps> = ({ params }) => {
           <Link href={`/togetherpay/${params.id}/recent`}>Recent activity</Link>
         </div>
       </div>
-      <div>Transactions</div>
-    </>
+      <div className="bg-navy-50 w-full h-1/2 px-4 flex flex-col items-center gap-7 justify-center">
+        <div className="mt-5 text-navy-200 w-full md:w-4/12 border-b-2 border-navy-200 pb-1 flex items-center gap-3">
+          <Search className="w-4 h-4" />
+          <input
+            type={"text"}
+            placeholder={"Search transactions"}
+            className="bg-navy-50 w-full"
+          />
+        </div>
+        <div className="mb-5 flex flex-col gap-3 justify-center items-center w-full md:w-4/12">
+          {displayData}
+        </div>
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/ExpenseDetail.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import Headline5 from "./ui/headlines/Headline5";
+
+interface ExpenseDetailProps {
+  type: string;
+  total: number;
+  memberName: string;
+  description: string;
+  date: string;
+}
+
+const ExpenseDetail: FC<ExpenseDetailProps> = ({
+  type,
+  total,
+  memberName,
+  description,
+  date,
+}) => {
+  console.log(type);
+  return (
+    <div className="bg-white text-black w-full rounded capitalize py-5 px-3 md:px-6 flex items-start justify-between">
+      <div>
+        <Headline5>{type}</Headline5>
+        <p className="text-sm">
+          {memberName} has paid for {description}
+        </p>
+      </div>
+      <div className="flex flex-col items-end">
+        <p className="text-brand-700 font-semibold">
+          {Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          }).format(total)}
+        </p>
+        <p className="text-xs">{date}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseDetail;
